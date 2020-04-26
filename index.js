a9c48768c1a523542585142e55579e7b85f0a068
Matias Oliva <olivamati@gmail.com>
2020-04-26 20:48:43 -0300

    add first response

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get('/hola/:name', (req, res) => {
+    res.send({ message: `hola ${req.params.name}` });
+})
+
 app.listen(port, () => {
     console.log(`API REST running in http://localhost:${port}`);
 })

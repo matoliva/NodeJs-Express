bdff3b51b7fc96c4ae55cc800feb673bbd8a329a
Matias Oliva <olivamati@gmail.com>
2020-05-02 20:36:44 -0300

    add get and post examples

--- index.js
+++ index.js
@@ -9,8 +9,24 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/hola/:name', (req, res) => {
-    res.send({ message: `hola ${req.params.name}` });
+app.get('/api/product', (req, res) => {
+    res.status(200).send({ products: []});
+})
+app.get('/api/product/:productId', (req, res) => {
+   
+})
+
+app.post('/api/product', (req, res) => {
+    console.log(req.body);
+    res.status(200).send({ message: 'it\'s ok' })
+})
+
+app.put('/api/product/:productId', (req, res) => {
+    
+})
+
+app.delete('/api/product/:productId', (req, res) => {
+
 })
 
 app.listen(port, () => {

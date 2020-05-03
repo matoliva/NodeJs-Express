2ee32874191d5a8503df2088cca5d5d0728f4dc8
Matias Oliva <olivamati@gmail.com>
2020-05-02 21:27:18 -0300

    Feature/crud base (#1)
    * add get and post examples
    
    * add mongoose dependency

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,10 +10,33 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/hola/:name', (req, res) => {
-    res.send({ message: `hola ${req.params.name}` });
+app.get('/api/product', (req, res) => {
+    res.status(200).send({ products: []});
+})
+app.get('/api/product/:productId', (req, res) => {
+   
 })
 
-app.listen(port, () => {
-    console.log(`API REST running in http://localhost:${port}`);
+app.post('/api/product', (req, res) => {
+    console.log(req.body);
+    res.status(200).send({ message: 'it\'s ok' })
+})
+
+app.put('/api/product/:productId', (req, res) => {
+    
+})
+
+app.delete('/api/product/:productId', (req, res) => {
+
+})
+
+mongoose.connect('mongodb://localhost:27017/shop', (err, resp) => {
+    if(err) {
+        return console.log(`Error al conectar a la base de datos: ${err}`);
+    }
+    console.log('Conexión con la base de datos establecida');
+    
+    app.listen(port, () => {
+        console.log(`API REST running in http://localhost:${port}`);
+    })
 })
